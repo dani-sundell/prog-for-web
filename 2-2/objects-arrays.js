3de2ef22ebd461d65ec8be530c5a91f553f3a0e1
dani-sundell <daniellesundell@gmail.com>
2022-01-27 20:18:15 -0800

    added semicolon

--- 2-2/objects-arrays.js
+++ 2-2/objects-arrays.js
@@ -5,4 +5,4 @@ const myFavGames = [
 ];
 
 const yourInquiry = window.prompt('I have 3 games in my collection. Pick a number between 1 and 3 and I will tell you about it.');
-window.alert('You selected ' + myFavGames[yourInquiry - 1].title + ' which is a ' + myFavGames[yourInquiry - 1].type + ' where ' + myFavGames[yourInquiry - 1].shortDescription + '.' + ' Up to ' + myFavGames[yourInquiry - 1].numberOfPlayers + ' people can play at once. I would rate it ' + myFavGames[yourInquiry - 1].rating + '.')
+window.alert('You selected ' + myFavGames[yourInquiry - 1].title + ' which is a ' + myFavGames[yourInquiry - 1].type + ' where ' + myFavGames[yourInquiry - 1].shortDescription + '.' + ' Up to ' + myFavGames[yourInquiry - 1].numberOfPlayers + ' people can play at once. I would rate it ' + myFavGames[yourInquiry - 1].rating + '.');
