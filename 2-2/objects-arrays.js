2a5c44ff2a387785401f0f174785eb5e076c4f08
dani-sundell <daniellesundell@gmail.com>
2022-01-27 20:15:30 -0800

    added 2-2

---
+++ 2-2/objects-arrays.js
@@ -0,0 +1,8 @@
+const myFavGames = [
+    {title: 'Wingspan', type: 'board game', numberOfPlayers: 5, rating: '5 stars', shortDescription: 'players build an aviary with different types of birds'},
+    {title: 'Settlers of Catan', type: 'board game', numberOfPlayers: 4, rating: '5 stars', shortDescription: 'players compete for resouces to build the best community'},
+    {title: 'Ticket to Ride', type: 'board game', numberOfPlayers: 6, rating: '4 stars', shortDescription: 'players use trains to build routes across the U.S'}
+];
+
+const yourInquiry = window.prompt('I have 3 games in my collection. Pick a number between 1 and 3 and I will tell you about it.');
+window.alert('You selected ' + myFavGames[yourInquiry - 1].title + ' which is a ' + myFavGames[yourInquiry - 1].type + ' where ' + myFavGames[yourInquiry - 1].shortDescription + '.' + ' Up to ' + myFavGames[yourInquiry - 1].numberOfPlayers + ' people can play at once. I would rate it ' + myFavGames[yourInquiry - 1].rating + '.')
