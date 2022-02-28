1b60ba480284805e1a6bcb3fd2b067071b9a33ae
dani-sundell <daniellesundell@gmail.com>
2022-02-27 17:29:39 -0800

    6-a commit

---
+++ 6-a/vue.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#myLlamas",
+    data: {
+      llamaFact1: "Llamas live to be about 20 years old. Though some only live for 15 years and others live to be 30 years old.",
+      llamaFact2: "Llamas are vegetarians and have very efficient digestive systems.",
+      llamaFact3: "Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white.",
+      llamaFact4: "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.",
+      llamaFact5: "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels.",
+      llamaFact6: "A full-grown llama weighs about 300-350 pounds",
+      llamaFact7: "A llama can spit green, partially digested food 15 feet or more",
+      llamaFact8: "Some llamas can grow up to 6 feet tall.",
+      llamaFact9: "Llamas eat approximately 2 to 4 percent of their body weight per day.",
+      llamaFact10: "Llama babies are called crias.",
+      llamaFact11: "The gestation period of a llama is 11.5 months (350 days)",
+      llamaBabies: false
+    }
+  });
