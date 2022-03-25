322a462923cdae3df93e64c3d78da76ed5a79ddf
dani-sundell <daniellesundell@gmail.com>
2022-03-24 18:10:35 -0700

    Week 9 exercises commit

---
+++ 9-2/camelids/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import CamelidRow from "../src/components/CamelidRow.vue"
+
+const  camelids = [
+      {
+        commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
+      },
+    ]
+</script>
+
+<template>
+  <div>
+    <h1>Camelids</h1>
+    <div class="container">
+      <CamelidRow v-for="critter in camelids" v-bind:key="critter.commonName" v-bind:animal="critter" />
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ 9-2/camelids/src/components/CamelidRow.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps(["animal"]);
+console.log(props.animal);
+</script>
+<template>
+    <div>
+      <h3>{{props.animal.commonName}}</h3>
+      <p>Binomial Name: {{props.animal.binomialName}}</p>
+      <p>Family: {{props.animal.family}}</p>
+      <p>Genus: {{props.animal.genus}}</p>
+      <p>Species: {{props.animal.species}}</p>
+      <img v-bind:src="props.animal.image" v-bind:alt="props.animal.commonName">
+    </div>
+</template>
+<style scoped>
+div {
+  max-width: 250px;
+  border: 1px solid black;
+  padding: 20px;
+  margin: 20px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+img {
+  max-width: 250px;
+  display: block;
+}
+h3 {
+  font-size: 24px;
+  color: forestgreen;
+}
+</style>
