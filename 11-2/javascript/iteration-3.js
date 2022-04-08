64195d935d9f4ed59abd328c6f457ea955360e30
dani-sundell <daniellesundell@gmail.com>
2022-04-07 21:50:08 -0700

    11-2 commit

---
+++ 11-2/javascript/iteration-3.js
@@ -0,0 +1,39 @@
+let x;
+let y;
+
+function centerCanvas() {
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+}
+
+//center canvas method source: https://github.com/processing/p5.js/wiki/Positioning-your-canvas
+
+function setup() {
+    cnv = createCanvas(500, 500);
+    centerCanvas();
+    background('#b067ef');
+    noStroke();
+    angleMode(DEGREES);
+    noLoop();
+}
+
+function createArch(x, y, width, height) {
+    fill(0, 0, 0, 50)
+    noStroke();
+    arc(x, y, width, width, 180, 0);
+    rect(x - width/2, y, width, height)
+}
+
+function draw() {
+    x = 250; 
+    y = 400;
+    width = 100; 
+    height = 100;
+    for (var i = 0; i < 7; i++) {
+        createArch(250, y, width, height);
+        y -= 30;
+        width += 400;
+        height += 10;
+    }
+}
